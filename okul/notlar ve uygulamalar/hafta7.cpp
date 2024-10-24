7e26a18bd6bc21192e96e581f5382301d9377165
ced3j <spoonnoisthere@gmail.com>
2024-10-24 11:13:03 +0300

    hafta7

---
+++ okul/notlar ve uygulamalar/hafta7.cpp
@@ -0,0 +1,16 @@
+/*
+
+Classlar
+
+
+    Değişkenler private tanımlanır
+    Üye fonksiyonlar publictir
+    Bir class'ın tüm üyeleri default olarak private olur
+
+    Sınıf nesnelerini dizi olarak kullanmak mümkün [Arrays of class objects]
+
+    Data abstraction, classes and abstract data types
+
+    information hiding
+            Ayrı header ve cpp dosyaları vs.
+*/
