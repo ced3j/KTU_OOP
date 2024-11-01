66cbca6b051b7f8a8f605c69463b411f4b233bde
ced3j <spoonnoisthere@gmail.com>
2024-11-01 11:18:32 +0300

    hafta9 konulari

---
+++ okul/notlar ve uygulamalar/hafta9.cpp
@@ -0,0 +1,46 @@
+// Arrayler, matrisler(iki ve daha çok boyut) classlar, enums
+// Matrisin tersini alma
+// Matriste swap işlemleri
+// stringleri matriste tutmak 
+// enum type
+/*
+
+
+double sales[10][5]
+
+int board[4][3] = {
+    {2,3,1},
+    {5,10,2},
+    {6,1,2},
+    {5,2,3}
+};
+
+
+const int row = 6;
+const int colms = 5;
+
+enum carType{GM, Ford, Toyoda, BMW, Nissan, Volvo}; // satırlar
+enum colorType{Red, Brown, Black, White, Gray}   // sütunlar
+
+int inStock[rows][colms];
+
+
+// aşağıdaki iki işlem de aynı işlemdir
+inStock[1][3] = 15;
+inStock[Ford][White] = 15;
+
+
+int table[][5]  // satır boş bırakılabilir
+
+
+enum tpyeName{value1, value2 .... };
+value1 < value2 < value3 ...
+
+enum colors{brown, blue, red, greed, yellow};
+enum konumBilgisi{freshman, sophomore, junior, senior};
+
+
+
+*/
+
+
